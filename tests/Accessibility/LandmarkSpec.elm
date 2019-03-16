220f99fa90063a59695aa389b42001737253e755
Anton Strömkvist <anton@stromkvist.com>
2019-03-16 22:33:20 +0100

    Re-add tests

---
+++ tests/Accessibility/LandmarkSpec.elm
@@ -0,0 +1,39 @@
+module Accessibility.LandmarkSpec exposing (spec)
+
+import Accessibility.Styled as Html
+import Accessibility.Styled.Landmark exposing (..)
+import Html.Attributes
+import Html.Styled exposing (toUnstyled)
+import Json.Encode
+import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector as Selector
+
+
+spec : Test
+spec =
+    describe "Accessibility.Landmark" <|
+        List.map (\( a, b ) -> addsRole a b)
+            [ ( application, "application" )
+            , ( banner, "banner" )
+            , ( complementary, "complementary" )
+            , ( contentInfo, "contentinfo" )
+            , ( form, "form" )
+            , ( main_, "main" )
+            , ( navigation, "navigation" )
+            , ( region, "region" )
+            , ( search, "search" )
+            ]
+
+
+addsRole : Html.Attribute Never -> String -> Test
+addsRole role_ expected =
+    test ("sets the role attribute: " ++ expected) <|
+        \() ->
+            Html.div [] [ Html.div [ role_ ] [] ]
+                |> toUnstyled
+                |> Query.fromHtml
+                |> Query.has
+                    [ Selector.attribute <|
+                        Html.Attributes.property "role" (Json.Encode.string expected)
+                    ]
